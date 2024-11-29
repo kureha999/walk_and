e031605ac07167b2c770c2d87a9a6146e4f48ada
めっちゃくれは <metchakureha@kurehapro3.local>
2024-11-30 00:02:49 +0900

    DeviseとLIENログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,25 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :set_header
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  private
+3
+
+  def set_header
+    if user_signed_in?
+      @header_partial = "shared/after_header"
+    else
+      @header_partial = "shared/header"
+    end
+  end
+
+  # Deviseのパラメータにnameを許可する
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
+  end
 end
