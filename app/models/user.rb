b0b7b6ec1ab2cf3457ef7c1f818085935d4b273a
めっちゃくれは <metchakureha@kurehapro3.local>
2024-11-30 14:20:16 +0900

    ログイン回りのcss

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
     social_profiles.select { |sp| sp.provider == provider.to_s }.first
   end
 
+  # 認証情報（omniauth）を基にオブジェクトの属性を設定
   def set_values(omniauth)
     return if provider.to_s != omniauth["provider"].to_s || uid != omniauth["uid"]
     credentials = omniauth["credentials"]
@@ -20,6 +21,7 @@ class User < ApplicationRecord
     name = info["name"]
   end
 
+  # プロバイダーから取得した生データを保存する
   def set_values_by_raw_info(raw_info)
     self.raw_info = raw_info.to_json
     self.save!
